6bf931903f0baaef326845f543d2698b547508a7
David Samerson <samerson10@gmail.com>
2018-07-27 11:28:16 -0300

    Novo projeto - Festa de fim de ano - UDEMY

---
+++ FestaFimDeAno/app/src/main/java/br/com/samerson/festafimdeano/festafimdeano/MainActivity.java
@@ -0,0 +1,37 @@
+package br.com.samerson.festafimdeano.festafimdeano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = (TextView) findViewById(R.id.text_today);
+        this.mViewHolder.textDaysLeft = (TextView) findViewById(R.id.text_days_left);
+        this.mViewHolder.buttonConfirm = (Button) findViewById(R.id.button_confirm);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.button_confirm) {
+            //execulta a lógica
+        }
+    }
+
+    private static class ViewHolder {
+
+        TextView textToday;
+        TextView textDaysLeft;
+        Button buttonConfirm;
+    }
+}
