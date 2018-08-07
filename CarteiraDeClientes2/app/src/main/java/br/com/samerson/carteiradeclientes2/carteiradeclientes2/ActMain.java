6c184955fcb45159355367b2f713d46e7e68e388
David Samerson <samerson10@gmail.com>
2018-08-07 16:29:51 -0300

    inicio do video 11

--- CarteiraDeClientes2/app/src/main/java/br/com/samerson/carteiradeclientes2/carteiradeclientes2/ActMain.java
+++ CarteiraDeClientes2/app/src/main/java/br/com/samerson/carteiradeclientes2/carteiradeclientes2/ActMain.java
@@ -1,6 +1,8 @@
 package br.com.samerson.carteiradeclientes2.carteiradeclientes2;
 
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,30 +10,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import br.com.samerson.carteiradeclientes2.carteiradeclientes2.Database.DadosOpenHelper;
+
 public class ActMain extends AppCompatActivity {
+
+    private SQLiteDatabase conexao;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+    private DadosOpenHelper dadosOpenHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(this.toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                Intent it = new Intent(ActMain.this, ActCadCliente.class);
-//                startActivity(it);
-//
-//            }
-//        });
+        this.fab = (FloatingActionButton) findViewById(R.id.fab);
 
     }
 
-    public void cadastrar (View v) {
+    private void criarConexao() {
+
+        //bloco de código de testes
+        try {
+
+            
+
+        } catch (SQLException ex) {
+
+        }
+    }
+
+    public void cadastrar(View v) {
 
         Intent it = new Intent(ActMain.this, ActCadCliente.class);
         startActivity(it);
